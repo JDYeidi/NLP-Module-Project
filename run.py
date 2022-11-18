22cc7b72c48410118be01971f3220aab8246191d
JDYeidi <96556497+JDYeidi@users.noreply.github.com>
2022-11-17 21:58:40 -0600

    Update run.py

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ from task_3 import translate_blue
 
 if __name__ == '__main__':
     print("Task 1: Out of the Box Sentiment Analysis ")
-    #task_1 = movies_reviews()
-    #task_1.main()
+    task_1 = movies_reviews()
+    task_1.main()
     print("-----------END------------")
     print("Task 2: Take a basic, pretrained NER model, and train further on a task-specific dataset")
     samples_train = 300
     samples_test = 30
-    #task_2 = model_trainer(samples_test, samples_train)
-    #task_2.main()
-    #task_2.graphic_model()
+    task_2 = model_trainer(samples_test, samples_train)
+    task_2.main()
+    task_2.graphic_model()
     print("-----------END------------")
     print("Task 3:Set up and compare model performance of two different translation models")
     lang1_set = 'en_corpus.txt'
